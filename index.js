9d906d7b6049a4387c58315394979af1fb82e66a
Azeez Yusuf Olamilekan <highshow74@gmail.com>
2022-09-09 12:40:25 +0100

    Adjusted and changed some contents in index.js file

--- index.js
+++ index.js
@@ -1,97 +1,86 @@
-const myLibrary = [];
+let myLibrary = [];
+let index = 0;
 const addNewBookBtn = document.querySelector('.new-book');
 const addBookBtn = document.querySelector('.add-book');
 const table = document.querySelector('tbody');
 
 function Book(author, title, pages, read) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
+  return {author, title, pages, read};
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+
 }
 
 function displayForm() {
   const form = document.querySelector('#form');
   form.style.display = 'block';
-  addNewBookBtn.style.display = 'none'
+  addNewBookBtn.style.display = 'none';
 }
 
 function displayBookPage() {
-  const row = table.insertRow();
-
-  for (const key in myLibrary.at(-1)) {
-    const value = myLibrary.at(-1)[key];
-    const cell = row.insertCell();
-    cell.innerHTML = value;
+  let text = '';
+  
+  let index = 0;
+  for (let i = 0; i < myLibrary.length; i++) {
+    index++;
+    const book = myLibrary[i];
+    text += `<tr data-index='${i}'>`;
+    text += `<td>${book.author}</td>`; 
+    text += `<td>${book.title}</td>`;
+    text += `<td>${book.pages}</td>`;
+    text += `<td>${book.read}</td>`;
+    text += `<td><button onclick='removeBook()' data-index='${i}' type='button' class='removeBtn'>X</button></td>`
+    text += `</tr>` 
   }
+  table.innerHTML = text;
+}
 
-  const cell = row.insertCell();
-  const removeBtn = document.createElement('button');
-  removeBtn.innerText = 'X';
-  removeBtn.setAttribute('style',
-    `font-size: 1rem; 
-    background-color: red; 
-    padding: 0.2rem 1.3rem 0; 
-    font-weight: bolder; cursor: pointer;`);
+function removeBook() {
+  confirm('Are You Sure?');
+  const removed = document.querySelector('#removed');
+  removed.style.display = 'block';
+  setTimeout(() => {
+    removed.style.display = 'none';
+  }, 1500);
 
-  function removeBook() {
-    confirm('Are You Sure?');
-    const removed = document.querySelector('#removed');
-    removed.style.display = 'block';
-    setTimeout(() => {
-      removed.style.display = 'none';
-    }, 1500);
-    row.remove();
-  }
-
-  removeBtn.addEventListener('click', removeBook);
-  cell.appendChild(removeBtn);
+  rows = document.querySelectorAll('tr');
+  rows.forEach((row)=> row.addEventListener('click', function (e) {
+    index = e.target.getAttribute('data-index');
+    myLibrary.splice(index, 1);
+    displayBookPage();
+  }))
 }
 
 function addToBookInfo() {
-  let author = document.querySelector('#author');
-  let title = document.querySelector('#title');
-  let pages = document.querySelector('#pages');
-  let read = document.querySelector('input[name=read]:checked');
+  let author = document.querySelector('#author').value;
+  let title = document.querySelector('#title').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('input[type=radio]:checked').value;
   const form = document.querySelector('#form');
 
-  author = author.value;
-  title = title.value;
-  pages = pages.value;
-  read = read.value;
-
   form.style.display = 'none';
-  addNewBookBtn.style.display = 'block'
+  addNewBookBtn.style.display = 'block';
 
   const added = document.querySelector('#added');
   added.style.display = 'block';
-  added.style.display = 'none';
+  setTimeout(() => {
+    added.style.display = 'none';
+  }, 1500);
 
-  const book = new Book(author, title, pages, read);
-  
-    addBookToLibrary(book);
-    displayBookPage();
+  const book = Book(author, title, pages, read);
+  addBookToLibrary(book);
+  displayBookPage();
 }
 
 addNewBookBtn.addEventListener('click', displayForm);
 addBookBtn.addEventListener('click', addToBookInfo);
 
-const book1 = new Book('Chinua Achebe', 'Things Fall Apart', 225, 'Yes');
-addBookToLibrary(book1);
-displayBookPage();
-
-const book2 = new Book('Elie Wiesel', 'Night', 116, 'No');
-addBookToLibrary(book2);
-displayBookPage();
-
-const book3 = new Book('Ola Rotimi', 'The Gods Are Not To Blame', 72, 'No');
-addBookToLibrary(book3);
-displayBookPage();
-
-const book4 = new Book('Keye Abiona', 'Even Kins Are Guilty', 85, 'Yes');
-addBookToLibrary(book4);
+myLibrary = [
+  Book('Chinua Achebe', 'Things Fall Apart', 225, 'Yes'),
+  Book ('Elie Wiesel', 'Night', 116, 'No'),
+  Book('Ola Rotimi', 'The Gods Are Not To Blame', 72, 'No'),
+  Book('Keye Abiona', 'Even Kins Are Guilty', 85, 'Yes')
+];
 displayBookPage();
